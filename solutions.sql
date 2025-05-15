504d1da301f20962e8adfff7a40c3bc5e0901bea
Gipnokot <36129002+Gipnokot@users.noreply.github.com>
2025-05-15 19:44:50 +0500

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -3,3 +3,20 @@
 SELECT name
 FROM Passenger;
 
+-- Вывести названия всеx авиакомпаний
+
+SELECT name
+FROM Company
+
+-- Вывести все рейсы, совершенные из Москвы
+
+SELECT *
+FROM Trip
+WHERE town_from = "Moscow"
+
+-- Вывести имена людей, которые заканчиваются на "man"
+
+SELECT name
+FROM Passenger WHERE name LIKE '%man'
+
+-- 
